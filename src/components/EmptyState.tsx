be09b6878d17b8a05ed5ae563e8845e5a4efabfd
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2025-05-03 11:45:19 -0500

    Overflow hidden

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -2,7 +2,7 @@ import { Empty } from "./Icons"
 
 export function EmptyState() {
  return (
-  <div className="flex flex-col items-center justify-center h-screen">
+  <div className="flex flex-col items-center justify-center h-screen overflow-hidden">
    <Empty />
    <p className="text-slate-800 text-center mt-8">You haven&apos;t created any notes yet!</p>
   </div>
